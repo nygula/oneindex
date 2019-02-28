d88ea8fa7a9fefd88219754b84d692ef38e58447
nygula <569878931@qq.com>
2019-02-28 16:24:43 +0800

    Signed-off-by: nygula <569878931@qq.com>

---
+++ config/base.php
@@ -0,0 +1,103 @@
+<?php return array (
+  'site_name' => '荒火教',
+  'password' => 'z0.123456',
+  'style' => 'material',
+  'onedrive_root' => '/share/',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'eeMONUC842@:?jwataOM78_',
+  'client_id' => '42554313-8b3d-4096-9ba9-87e706a45dd5',
+  'redirect_uri' => 'https://ju.tn/',
+  'refresh_token' => 'OAQABAAAAAACEfexXxjamQb3OeGQ4GugvJqI_mzW1pMOzcO3CJLOkIwsNfLWbJTt2SX3ZjG3Z7V5TYOlXQnoa-TZiHpZ-v_S6zwFhdYCd8F4gfPgFYOmRr5Kb_K5D3kAnt5jYVhhmIZWO8RzDcAQ0BZIPQgRgAKMc-8-lcxKTOoE9PTv0-9_w3ej3jnv7SW1n-UjqMROtm3wFj9ClOjHGvMfM9Gg8EBniGHSfESoT4YlrRSMgOpkP9E-j6D1IPWX8dsNYf4jEp_yOLqQvbiAa38n62EeRIU8Xv9_qz7vgwS6OgYrutz-arr6xR3oyBusIJNTT6tMStMMxDJB33DanRy79yYnA-1QvIcx8NdaiCZB5jILx8pQJ_y1PkF-154Mjv2Tsu0zNhSjfYQchFQl8eTm7kjljRVmkRAA9GnGh5LKLpzPGrTkPkhwA7VDxyJj_v6Cru3vzeO1A517kbWHX8D1o43swDOzC-fUJqxo-wX0FyfD1yZ0H8W8rfsZwXJKKUBZ5KbKOe2A83FPx_EOQ6r1qJy6Y4brdrEB9j21edJpvlHGhX-dhPMZUWDGqsn0yH_AhLgMlwA-80O01LDTTm11CEFDmUWh2CnCEho38fnst47OIBFXdZagMFxAGRYA1tB1DJ26AI7EoKzBCukYemTBGpIcBPdGBvvsLGFVhzg8Lmb-9fn9gRQEjL1t4yJOEa00yPkwczFkP2D2vFrnSiIcw8TxphuVpnkoB4h0fo6yJBKxUYOyP3g-lj3_sI5NVwl50mPC3ABaeE9yhVDcBpt-uiImRcBdq-ndsRrpyfBew15Vr4us-NyAA',
+);
